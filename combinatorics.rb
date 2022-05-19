4c4b162320a1fe80d837aa46d63c6b003c946cc5
Kwanda Silekwa <46996545+KwandaSilekwa@users.noreply.github.com>
2022-05-19 20:12:27 +0200

    Create combinatorics.rb

---
+++ combinatorics.rb
@@ -0,0 +1,14 @@
+function CombinatoricsChallenge(num) { 
+
+  function fibonacci_num(num){
+    if (num <= 1)
+      return num;
+    return fibonacci_num(num -1) + fibonacci_num(num - 2);
+  }
+
+  num = fibonacci_num(num + 1);
+  return num; 
+
+}   
+// keep this function call here 
+console.log(CombinatoricsChallenge(readline()));
